3494573c0ca6dfbd36d59d933c6daa12d81c652a
CaptenSepp <sepp.hashemi.it8@gmail.com>
2022-05-30 12:11:33 +0200

    provider deleted from myApp

--- lib/core/my_app.dart
+++ lib/core/my_app.dart
@@ -1,26 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:hive_training_johannes/core/provider/my_app_provider.dart';
-import 'package:hive_training_johannes/page/transaction_page.dart';
-import 'package:provider/provider.dart';
+
+import '../screens/transaction_page.dart';
 
 class MyApp extends StatelessWidget {
-  static final String title = 'Hive Expense App';
-
   const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) => ChangeNotifierProvider(
-      create: (_) => MyAppProvider(), child: const _MyApp());
-}
-
-class _MyApp extends StatelessWidget {
-  const _MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      // title: title,
       theme: ThemeData(primarySwatch: Colors.brown),
       home: TransactionPage(),
     );
